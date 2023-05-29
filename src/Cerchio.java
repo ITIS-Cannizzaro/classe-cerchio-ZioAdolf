663c3a038a331d68e337ed8b35cf0d34b31d4e82
glist <glist@192.168.1.9>
2023-05-29 18:24:12 +0200

    Consegna prova

--- src/Cerchio.java
+++ src/Cerchio.java
@@ -1,4 +1,32 @@
 
 public class Cerchio {
-
+	private double r;
+	private double area;
+	private double diametro;
+	private double perimetro;
+	public Cerchio(double r) {
+		this.r= 0;
+	}
+	public void calcolaDiametro(double r) {
+		System.out.println(r*2); 
+	}
+	public void calcolaPerimetro(double r) {
+		System.out.println((2*Math.PI)*r);
+	}
+	public void calcolaArea(double r) {
+		System.out.println(Math.PI*r*r); 
+	}
+	public double getArea() {
+		return area;
+	}
+	public double getDiametro() {
+		return diametro;
+	}
+	public double getPerimetro() {
+		return diametro;
+	}
+	
+	public String toString() {
+		return "Area: "+area+"\n"+"Diametro: "+diametro+"\n"+"Perimetro: "+perimetro;
+	}
 }
--- src/UsoCerchio.java
+++ src/UsoCerchio.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class UsoCerchio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double n = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		Cerchio MJ = new Cerchio(n);
+		System.out.println("Inserire il raggio: ");
+		n = sc.nextInt();
+		
+		System.out.println("Area: ");
+		MJ.calcolaArea(n);
+		System.out.println("Diametro: ");
+		MJ.calcolaDiametro(n);
+		System.out.println("Perimetro: ");
+		MJ.calcolaPerimetro(n);
+		
 	}
 
 }
